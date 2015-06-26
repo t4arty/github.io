3a2d2648aa39db21df077758f34ebb0fb376fcf6
t4arty <t4arty@yandex.ru>
2015-06-26 19:51:41 +0300

    UI changes
    added ui item

--- Main.js
+++ Main.js
@@ -2,14 +2,20 @@
  * 
  */
 
+var u;
+
+
 VK.init({
     apiId: 1755175
 });
 	
 VK.api("users.get",{user_ids:"1,2,3"}, function(data){
-    
+    u = data.response.id;
 });
 
+function test() {
+    return u;
+}
 
 function showAdr(str) {
 	if (str == '' || str == ' ') {
