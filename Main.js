8f3ac58f3f98a1f6b5cc049fe2682cf4b4508772
t4arty <t4arty@yandex.ru>
2015-06-26 23:52:32 +0300

    chages

--- Main.js
+++ Main.js
@@ -9,13 +9,14 @@ VK.init({
 
 var u = 11;
 
-function test1() {
-    VK.api("wall.getById",{posts:'-45091870_69859',}, function(data){
-	for(i=0;i<data.response.length;i++) {
-	    u = data.response[i].text;
+function getGroupInfo() {
+    VK.api("groups.getById",{'group_id':'45091870','fields':'members_count,photo_100'}, function(data){
+	for(key in data.response) {
+	    u = key;
 	    document.getElementById('count').innerHTML += u + '<br>';
 	}
     });
 }
 
 //'http://vk.com/darcor?w=wall-45091870_69859'
+//  45091870 
