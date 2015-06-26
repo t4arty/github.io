60f214d737d38f9b3e83f824c2f41b34b92c6714
t4arty <t4arty@yandex.ru>
2015-06-26 20:28:33 +0300

    changes

--- Main.js
+++ Main.js
@@ -2,26 +2,17 @@
  * 
  */
 
-var u='work';
-
-
 VK.init({
     apiId: 1755175
 });
-	
-VK.api("users.get",{user_ids:"1,2,3"}, function(data){
-    u = data.response.id;
-    console.log(u);
-});
+
+
+var u = 11;
 
 function test1() {
-    document.getElementById('count').innerHTML = u;
+    VK.api("users.get",{user_ids:"1,2,3"}, function(data){
+	u = data.response.id;
+	console.log(u);
+    });
 }
 
-function showAdr(str) {
-	if (str == '' || str == ' ') {
-		document.getElementById('test').innerHTML = ' ';
-	}else{
-		document.getElementById('test').innerHTML = str;
-	}
-}
