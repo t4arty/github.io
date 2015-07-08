f411d44b52550325e772350bb16bbc10b30faa89
t4arty <t4arty@yandex.ru>
2015-07-08 16:12:03 +0300

    changes

--- Main.js
+++ Main.js
@@ -63,7 +63,7 @@ function getGroupMembers(objTarget) {
     });*/
     console.log("post", aboutPost[0], aboutPost[1]);
 
-    code = 'var c=0;var co=1000;var p=[];var i=0;var o='+offset+';var u=[]; '+
+    code = 'var c=0;var co=100;var p=[];var i=0;var o='+offset+';var u=[];'+
         'while(i!=10) {var li=API.likes.getList({"type":"post","owner_id":'+aboutPost[0]+',"item_id":'+aboutPost[1]+',"offset":o,"count":co});'+
         'c=li.count;u=u+API.users.get({"user_ids":li.items,"fields":"sex"})@.sex;i=i+1;o=o+co;}'+
         'return {"count":c,"user":u,"le":u.length};';
