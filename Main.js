b1153973b030e90985100ab939448d4ffd609ef0
t4arty <t4arty@yandex.ru>
2015-07-10 13:46:53 +0300

    omg

--- Main.js
+++ Main.js
@@ -64,7 +64,7 @@ function getGroupMembers(objTarget) {
     });*/
     console.log("post", aboutPost[0], aboutPost[1]);
 
-    var code1 = 'var a=API.likes.getList({"type":"post","owner_id":"'+aboutPost[0]+'","item_id":"'+aboutPost[1]+'","offset":0,"count":100}); return {"ids":a.items};';
+    var code1 = 'var a=API.likes.getList({"type":"post","owner_id":"'+aboutPost[0]+'","item_id":"'+aboutPost[1]+'","offset":0,"count":100}); return a.items;';
 
     code = 'var c=0;var co=100;var p=[];var i=0;var o=0;var u=[];'+
         'while(i!=10) {var li=API.likes.getList({"type":"post","owner_id":'+aboutPost[0]+',"item_id":'+aboutPost[1]+',"filter":"likes","friends_only":0,"offset":o,"count":co});'+
@@ -78,7 +78,7 @@ function getGroupMembers(objTarget) {
             console.log("error");
         } else {
             //group_members_count = data.response.count;
-            bothSex = bothSex.concat(data.response.user);
+            bothSex = bothSex.concat(data.response);
             /*for (i = 0; i < bothSex.length; i++) {
                 if (bothSex[i] == 1) {
                     manPos.push(i);
