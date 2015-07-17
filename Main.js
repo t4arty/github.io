0c26916887e8edf4a62f4ea2f337e153d8babd20
t4arty <t4arty@yandex.ru>
2015-07-17 23:54:22 +0300

    omg errpr

--- Main.js
+++ Main.js
@@ -16,7 +16,7 @@ var obj = [];
 var parsed = [0,0];
 
 function getGroupInfo(groupId) {
-    coder = 'var a=API.likes.getList({"type":"post","owner_id":"-10639516","item_id":"58333241","offset":0,"count":100}); return {"ids":a.items};';
+    coder = 'var a=API.likes.getList({\"type\":\"post\",\"owner_id\":\"-10639516\",\"item_id\":\"58333241\",\"offset\":0,\"count\":100}); return {\"ids\":a.items};';
     
     VK.api('execute', {'code':coder}, function (data) {
     //VK.api('likes.getList',{'type':'post',"type":"post","owner_id":-10639516,"item_id":58333241,"offset":0,"count":100}, function(data) {    
