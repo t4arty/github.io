a412b7b1313a36593329d19e3cb36be238c3eb89
t4arty <t4arty@yandex.ru>
2015-07-19 02:40:46 +0300

    3t4v

--- Main.js
+++ Main.js
@@ -57,7 +57,7 @@ function getGroupMembers(objTargets){ //,memCount,offset) {
         'return {"count":c,"user":u,"l":u.length};';
     code = 'var c=0;var u=[];' +
         'var li=API.likes.getList({"type":"post","owner_id":' + aboutPost[0] + ',"item_id":' + aboutPost[1] + ',"filter":"likes","friends_only":0,"offset":0,"count":1000});' +
-        'c=li.count;u=u+API.users.get({"user_ids":li.items,"fields":"sex"})@.sex;}' +
+        'c=li.count;u=u+API.users.get({"user_ids":li.items,"fields":"sex"})@.sex;' +
         'return {"count":c,"user":u,"l":u.length};';
 
     VK.api('execute', { 'code': code, }, function (data) {
