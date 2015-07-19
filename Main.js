b7aeac490318bdb3bb1f5c551dbff53a7d35f715
t4arty <t4arty@yandex.ru>
2015-07-19 16:16:43 +0300

    fdsfvg'

--- Main.js
+++ Main.js
@@ -42,32 +42,26 @@ function getGroupInfo(groupId) {
     
     
     waiting();//wait
-    getGroupMembers(parsed);
+    getGroupMembers(parsed,0);
 }
 
 
 
-function getGroupMembers(objTargets){ //,memCount,offset) {
+function getGroupMembers(objTargets,offset){ //,memCount,offset) {
     var aboutPost = objTargets; // 0-group, 1-item_post
     var code = '';
-    console.log(aboutPost[0],aboutPost[1]);
-    /*code = 'var c=0;var co=1000;var p=[];var i=0;var o=0;var u=[];'+
+    code = 'var c=0;var co=1000;var p=[];var i=0;var o='+offset+';var u=[];'+
         'while(i!=10) {var li=API.likes.getList({"type":"post","owner_id":'+aboutPost[0]+',"item_id":'+aboutPost[1]+',"filter":"likes","friends_only":0,"offset":o,"count":co});'+
-        'c=li.count;u=u+API.users.get({"user_ids":li.items,"fields":"sex"})@.sex;i=i+1;o=o+co;}'+
-        'return {"count":c,"user":u};';*/
-    code = 'var c=0;var u=[];'
-        +'var li=API.likes.getList({"type":"post","owner_id":'
-        +'-10639516' + ',"item_id":' + '58468105' + ',"filter":"likes","friends_only":0,"offset":0,"count":1000});'
-        +'c=li.count;var s=API.users.get({"user_ids":li.users,"fields":"sex"}); u=u+s@.sex;'
-        +'return {"count":c,"user":u};';
-    //http://vk.com/mdk?w=wall-10639516_58468105
+        'c=li.count;u=u+API.users.get({"user_ids":li.users,"fields":"sex"})@.sex;i=i+1;o=o+co;}'+
+        'return {"count":c,"user":u};';
+    
     VK.api('execute', { 'code': code, }, function (data) {
-        console.log(data);
         if (data.response.error) {
             errorMSG('Wrong: Group Member');
             console.log("error");
         } else {
-            
+            sexArray = sexArray.concat(data.response.user);
+            console.log(sexArray);
         }
     });
 }
