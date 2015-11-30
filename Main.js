83748e208425167d948884fc9bfa9977d56fbc0b
t4arty <t4arty@yandex.ru>
2015-11-30 22:41:51 +0300

    new 2

--- Main.js
+++ Main.js
@@ -70,7 +70,9 @@ function getGroupInfo(groupId) {// ??? 2nd main funtion
 function getGroupMembers(objTargets, offset) {
 	//clear some vars
 	var sexCountArray = new Array[2];
-	var menpos = [], wompos = [], onopos = [];
+	var menpos = [];
+	var wompos = [];
+	var onopos = [];
 
 	var cMembers = 0;
 	var aboutPost = objTargets;
